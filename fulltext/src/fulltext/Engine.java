dea818708b0cc6c2a83b8399bb8bd338c9049a94
iBoxDB <>
2016-01-27 06:36:39 -0800

    Update

--- fulltext/src/fulltext/Engine.java
+++ fulltext/src/fulltext/Engine.java
@@ -1,8 +1,7 @@
 //Free
 package fulltext;
 
-import iBoxDB.LocalServer.*;
-import java.io.IOException;
+import iBoxDB.LocalServer.*; 
 import java.util.*;
 
 public class Engine {
@@ -40,6 +39,35 @@ public class Engine {
 
     public Iterable<KeyWord> search(final Box box, final String str) {
         char[] cs = sUtil.clear(str);
+        KeyWord kw = new KeyWord();
+        kw.isWord = true;
+        kw.setKeyWord(new String(cs).trim());
+        final Iterator<KeyWord> it = search(box, kw, null).iterator();
+        if (it.hasNext()) {
+            return new Iterable<KeyWord>() {
+                @Override
+                public Iterator<KeyWord> iterator() {
+                    return new Iterator<KeyWord>() {
+                        Iterator<KeyWord> self = null;
+
+                        @Override
+                        public boolean hasNext() {
+                            if (self == null) {
+                                self = it;
+                                return true;
+                            }
+                            return self.hasNext();
+                        }
+
+                        @Override
+                        public KeyWord next() {
+                            return it.next();
+                        }
+                    };
+                }
+            };
+        }
+
         KeyWord[] kws = util.fromString(-1, cs).values().toArray(new KeyWord[0]);
         return search(box, kws);
     }
@@ -70,11 +98,15 @@ public class Engine {
         if (kws.length == 1) {
             return search(box, kws[0], (KeyWord) null);
         }
+
         return search(box, kws[kws.length - 1],
-                search(box, Arrays.copyOf(kws, kws.length - 1)));
+                search(box, Arrays.copyOf(kws, kws.length - 1)),
+                kws[kws.length - 1].getPosition()
+                != (kws[kws.length - 2].getPosition() + 1));
     }
 
-    private Iterable<KeyWord> search(final Box box, final KeyWord nw, Iterable<KeyWord> condition) {
+    private Iterable<KeyWord> search(final Box box, final KeyWord nw,
+            Iterable<KeyWord> condition, final boolean isWord) {
         final Iterator<KeyWord> cd = condition.iterator();
         return new Iterable<KeyWord>() {
 
@@ -89,7 +121,9 @@ public class Engine {
                             return true;
                         }
                         if (cd.hasNext()) {
-                            r1 = search(box, nw, cd.next()).iterator();
+                            KeyWord c = cd.next();
+                            c.isWord = isWord;
+                            r1 = search(box, nw, c).iterator();
                         } else {
                             return false;
                         }
--- fulltext/src/fulltext/MainClass.java
+++ fulltext/src/fulltext/MainClass.java
@@ -1,10 +1,13 @@
 // http://www.iboxdb.com/
 package fulltext;
 
+import iBoxDB.LocalServer.DB;
+
 public class MainClass {
 
     public static void main(String[] args) throws Exception {
-
+        DB.root("/tmp/");
+ 
     }
 
 }
