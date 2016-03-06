9b39357e39a3663c3824652b834844d8082f87ed
iBoxDB <iboxdb@users.noreply.github.com>
2016-03-05 23:41:00 -0800

    FTS

--- fulltext/src/iBoxDB/MainClass.java
+++ fulltext/src/iBoxDB/MainClass.java
@@ -103,7 +103,7 @@ public class MainClass {
 
         try (Box box = auto.cube()) {
             //searchDistinct() , search()
-            for (KeyWord kw : engine.search(box, "版本控 订 任何类型")) {
+            for (KeyWord kw : engine.search(box, "版本控 BitKeeper")) {
                 System.out.println(kw.toFullString());
                 System.out.println(engine.getDesc(ts[(int) kw.getID()], kw, 20));
             }
